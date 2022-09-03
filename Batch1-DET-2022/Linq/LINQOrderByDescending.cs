3d1bae9121a0adb6270ec85082b6e6a312bd2c9d
Chandanagunaga <94128160+Chandanagunaga@users.noreply.github.com>
2022-09-03 20:54:39 +0530

    Add files via upload

---
+++ Batch1-DET-2022/Linq/LINQOrderByDescending.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BATCH1_DET_2022
+{
+    internal class LINQOrderByDescending
+    {
+        public static void Main()
+        {
+            string[] names = { "chandana", "saroja", "bharati","tara" };
+
+            var result = names.OrderByDescending(n => n);
+
+            Console.WriteLine("Descending ordered list of names:");
+            foreach (string name in result)
+                Console.WriteLine(name);
+        }
+    }
+}
---
+++ Batch1-DET-2022/Linq/LINQ_Skip.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BATCH1_DET_2022
+{
+    internal class LINQ_Skip
+
+    {
+        public static void Main()
+        {
+            string[] words = { "chandana", "roja", "mary", "sheela", "Manasa", "ananya" };
+
+            var result = words.Skip(4);
+
+            Console.WriteLine("Skips the first 4 words:");
+            foreach (string word in result)
+               Console.WriteLine(word);
+        }
+    }
+}
