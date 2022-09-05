4dc1009c5a5192de65f09da0d464a74a28ad110a
Chandanagunaga <94128160+Chandanagunaga@users.noreply.github.com>
2022-09-05 18:31:07 +0530

    Add files via upload

---
+++ EMP.cs
@@ -0,0 +1,31 @@
+﻿namespace MyClassLib;
+
+
+
+public class EMP
+{
+    int id;
+    string name;
+    string dept;
+    double sal;
+
+
+
+    public DateTime doj { get; set; }
+    //constructor - is a function used to init object data
+    //has the same name as classname, no ret type
+    public EMP(int eid, string ename, DateTime doj)
+    {
+        id = eid; name = ename; this.doj = doj;
+    }
+
+    //function written inside a class is known as method
+    public int GetYearsofExp()
+    {
+        return DateTime.Now.Year - doj.Year;
+    }
+}
+
+
+
+
